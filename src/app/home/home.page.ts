eea1c7dcaea6483e0cfd84eca2e138b2884da26b
Hsu-DBS <20084781@mydbs.ie>
2025-12-26 01:07:14 +0000

    :sparkles: Add geolocation feature to find nearby services

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [IonicModule, CommonModule],
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
+
   constructor() {}
+
+  async findNearbyService(service: string) {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+
+      const url = `https://www.google.com/maps/search/${service}/@${lat},${lng},15z`;
+      window.open(url, '_blank');
+
+    } catch (error) {
+      console.error('Error getting location', error);
+      alert('Unable to get location. Please enable location services.');
+    }
+  }
 }
